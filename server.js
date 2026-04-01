f520122e4d3ff32c257f9ed5de36596131f979da
pdv21 <phamviet2112@gmail.com>
2026-04-01 10:00:24 +0700

    Fix

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const app = require('./app');
 
 const PORT = process.env.PORT;
+console.log("ENV PORT:", process.env.PORT);
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
