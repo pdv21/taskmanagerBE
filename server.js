9a9d14800f1cf2ae9fee0dd0acb373ed57dfde05
pdv21 <phamviet2112@gmail.com>
2026-04-01 10:03:42 +0700

    Fix

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
-const app = require('./app');
 
-const PORT = process.env.PORT;
-console.log("ENV PORT:", process.env.PORT);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
 });
+
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION:", err);
+});
+
+try{
+  const app = require('./app');
+
+  const PORT = process.env.PORT;
+  console.log("ENV PORT:", process.env.PORT);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (err) {
+  console.error('Error starting server:', err);
+}
